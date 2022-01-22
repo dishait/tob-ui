e1259fa66639ca656242b894ad0a50c9137d0d7f
markthree <1801982702@qq.com>
2022-01-22 14:52:03 +0800

    review: pages.json

---
+++ pages/base/foo/foo.vue
@@ -0,0 +1,20 @@
+<template>
+    <view>
+        <t-nav-bar title="Foo" leftText="Home" sticky />
+
+        <example customClass="mt-8">
+            <view class="mx-2"></view>
+        </example>
+    </view>
+</template>
+
+<script>
+export default {
+    data() {
+
+    }
+}
+</script>
+
+<style>
+</style>
